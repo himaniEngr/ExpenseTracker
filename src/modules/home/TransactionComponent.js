1082d9dc1d1c51bb8d55071785d8189391c8ee11
Himani <himani9.engg@gmail.com>
2022-02-24 16:12:59 +0530

    listed transactions

--- src/modules/home/TransactionComponent.js
+++ src/modules/home/TransactionComponent.js
@@ -4,10 +4,45 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  font-family: montserrat;
-  margin: 30px 0 10px;
+  padding: 10px 22px;
+  font-size: 18px;
+  width: 100%;
+  gap: 10px;
+  font-weight: bold;
+  & input {
+    padding: 10px 12px;
+    border-radius: 12px;
+    background: #e6e8e9;
+    border: 1px solid #e6e8e9;
+    outline: none;
+  }
 `;
+const Cell = styled.div`
+  display: flex;
+  padding: 10px 15px;
+  font-size: 14px;
+  border-radius: 2px;
+`;
+
+const TransactionCell = (props) => {
+  return (
+    <Cell>
+      <span>{props.payload.desc}</span>
+      <span>${props.payload.amount}</span>
+    </Cell>
+  );
+};
 const TransactionComponent = (props) => {
-  return <Container>Transaction Component</Container>;
+  return (
+    <Container>
+      Transactions
+      <input placeholder="search" />
+      {props.transactions.length
+        ? props.transactions.map((payload) => {
+            return <TransactionCell payload={payload} />;
+          })
+        : ""}
+    </Container>
+  );
 };
 export default TransactionComponent;
